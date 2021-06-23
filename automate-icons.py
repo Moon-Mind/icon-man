d491e00b3fe46d5923f7f6e4735bd331af7bb2d5
Moon_Mind <52799853+Moon-Mind@users.noreply.github.com>
2021-06-23 03:10:19 +0200

    Update automate-icons.py

--- automate-icons.py
+++ automate-icons.py
@@ -1,20 +1,36 @@
+from os import X_OK, link, read
 import requests
 from bs4 import BeautifulSoup
-import json
 import urllib
+from PIL import Image 
+import os
 
+def downloader(a,b):
+    link = a
+    path = "Icons/"
+    count = str(b)
+    #link = "https://apps.apple.com/de/app/google-drive-dateispeicher/id507874739"
+    page = requests.get(link)
+    if page.status_code == 200:
+        content = page.content
+        Data = {}
+        DOMdocument = BeautifulSoup(content, 'html.parser')
+        Data['Image'] = DOMdocument.find("meta",  property="og:image")['content']
+        url = Data["Image"]
+        name = link[30:33]
+        urllib.request.urlretrieve(url, path+count+name+".png")
+        im = Image.open(path+count+name+".png")
+        im = im.crop((385, 100, 815, 530))
+        im.save(path+count+name+".png")
 
-page = requests.get("https://apps.apple.com/de/app/telegram-messenger/id686449807")
+    pass
 
-if page.status_code == 200:
-    content = page.content
+# Using readlines()
+file1 = open('links.txt', 'r')
+Lines = file1.readlines()
+count = 0
 
-    Data = {}
-
-    DOMdocument = BeautifulSoup(content, 'html.parser')
-
-    Data['Image'] = DOMdocument.find("meta",  property="og:image")['content']
-    print('Beginning file download with urllib2...')
-    print(Data["Image"])
-    url = Data["Image"]
-    urllib.request.urlretrieve(url, '1.png')
+# Strips the newline character
+for line in Lines:
+    count += 1
+    downloader(line.strip(),count)

7d2fbbeb9a512e159fdee30c29b9a4cad27a5aca
Moon_Mind <52799853+Moon-Mind@users.noreply.github.com>
2021-07-08 17:18:45 +0200

    Update Icon-downloader.py

--- Icon-downloader.py
+++ Icon-downloader.py
@@ -2,13 +2,17 @@ from os import X_OK, link, read
 import requests
 from bs4 import BeautifulSoup
 import urllib
+import numpy as np
 from PIL import Image 
 import os
 
+
+name = "test"
+
 def downloader(a,b):
     link = a
+    name = b
     path = "Icons/"
-    count = str(b)
     #link = "https://apps.apple.com/de/app/google-drive-dateispeicher/id507874739"
     page = requests.get(link)
     if page.status_code == 200:
@@ -17,20 +21,28 @@ def downloader(a,b):
         DOMdocument = BeautifulSoup(content, 'html.parser')
         Data['Image'] = DOMdocument.find("meta",  property="og:image")['content']
         url = Data["Image"]
-        name = link[30:33]
-        urllib.request.urlretrieve(url, path+count+name+".png")
-        im = Image.open(path+count+name+".png")
+
+        urllib.request.urlretrieve(url, path+name+".png")
+        im = Image.open(path+name+".png")
         im = im.crop((385, 100, 815, 530))
-        im.save(path+count+name+".png")
+        
+        im.save(path+name+".png")
+
 
     pass
 
+
 # Using readlines()
 file1 = open('links.txt', 'r')
+array_from_file = np.loadtxt("name.txt", dtype=str)
+
+        
 Lines = file1.readlines()
 count = 0
 
+
 # Strips the newline character
 for line in Lines:
+    name = array_from_file[count]
     count += 1
-    downloader(line.strip(),count)
+    downloader(line.strip(),name)
